58b3be34cc9d982d96b7e944a6272b2ab93739a9
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-26 15:33:09 -0300

    criação do botão de sign-in

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,31 @@
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import styles from "./styles.module.scss";
+
+export function SignInButton() {
+
+    const isUserLoggedIn = false;
+
+    return isUserLoggedIn ? (
+
+        <button 
+            type="button"
+            className={styles.signInGithub}
+        >
+            <FaGithub color="#04d361"/>
+            Paulo Henrique
+            <FiX color="#737380" className={styles.closeIcon}/>
+        </button>
+
+    ) : (
+
+        <button 
+            type="button"
+            className={styles.signInGithub}
+        >
+            <FaGithub color="#eba417"/>
+            Sign in with Github
+        </button>
+
+    );
+}
