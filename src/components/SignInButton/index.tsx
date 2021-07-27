5e7c048364206d0c7f6f2b11bc4038aa23aa3cb8
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-27 16:16:10 -0300

    autenticação com OAuth do GitHub completa

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut , useSession } from "next-auth/client";
+
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
 
-    const isUserLoggedIn = false;
+    const [session] = useSession();
 
-    return isUserLoggedIn ? (
+    return session ? (
 
         <button 
             type="button"
             className={styles.signInGithub}
+            onClick={() => signOut()}
         >
             <FaGithub color="#04d361"/>
-            Paulo Henrique
+            {session.user.name}
             <FiX color="#737380" className={styles.closeIcon}/>
         </button>
 
@@ -22,6 +25,7 @@ export function SignInButton() {
         <button 
             type="button"
             className={styles.signInGithub}
+            onClick={() => signIn("github")}
         >
             <FaGithub color="#eba417"/>
             Sign in with Github
