fb96daa15746028577b6776b10a625fb9ab010a6
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-28 15:07:18 -0300

    integracão completa com faunaDB e registro de sessão dentro do fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
+import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+
+import { fauna } from "../../../services/faunadb";
 
 export default NextAuth({
 
@@ -9,5 +12,42 @@ export default NextAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
             scope: "read:user"
         }),
-    ]
+    ],
+
+    callbacks: {
+        async signIn(user, account, profile){
+
+            const { email } = user;
+            try{
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index("user_by_email"),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection("users"),
+                            {data : {email} }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index("user_by_email"),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )
+                );
+
+                return true;
+            } catch {
+                return false;
+            }
+
+        }
+    }
+    
 });
