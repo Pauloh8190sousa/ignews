6ebcceeaf823b7418b2c5e081f2a5f0b1bab3b14
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-30 16:07:53 -0300

    manipulação de eventos com o stripe

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { Readable } from "stream";
 import Stripe from "stripe";
 import { stripe } from "../../services/stripe";
+import { saveSubscription } from "./_lib/manageSubscription";
 
 async function buffer(readable: Readable) {
     const chunks = [];
@@ -22,7 +23,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    "checkout.session.completed"
+    "checkout.session.completed",
+    "customer.subscriptions.created",
+    "customer.subscriptions.updated",
+    "customer.subscriptions.deleted",
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -42,7 +46,41 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const { type } = event;
 
         if(relevantEvents.has(type)) {
-            console.log("Evento recebido", event);
+
+            try{
+                switch(type) {
+
+                    case "customer.subscriptions.created":
+                    case "customer.subscriptions.updated":
+                    case "customer.subscriptions.deleted":
+                        
+                        const subscription = event.data.object as Stripe.Subscription;
+
+                        await saveSubscription(
+                            subscription.id,
+                            subscription.customer.toString(),
+                            type == "customer.subscriptions.created"
+                        )
+
+                        break;
+                    case "checkout.session.completed":
+
+                        const checkoutSession = event.data.object as Stripe.Checkout.Session
+
+                        await saveSubscription(
+                            checkoutSession.subscription.toString(),
+                            checkoutSession.customer.toString(),
+                            true
+                        );
+
+                        break;
+                    default:
+                        throw new Error("Unhandled event.");
+                }
+            }catch(err){
+                return res.json({error: "Webhook handler failed."})
+            }
+
         }
 
         res.json({received: true})
