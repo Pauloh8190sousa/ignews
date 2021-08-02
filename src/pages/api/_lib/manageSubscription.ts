97374677a63e20210a0faf3549280770ce91eb28
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-08-02 17:24:01 -0300

    página de posts 100% completa

--- src/pages/api/_lib/manageSubscription.ts
+++ src/pages/api/_lib/manageSubscription.ts
@@ -40,11 +40,20 @@ export async function saveSubscription(
             )
         );
     }else {
-    /*    await fauna.query(
+        await fauna.query(
             q.Replace(
-                
+                q.Select(
+                    "ref",
+                    q.Get(
+                        q.Match(
+                            q.Index("subscription_by_id"),
+                            subscriptionId
+                        )
+                    )
+                ),
+                {data: subscriptionData}
             )
-        )*/
+        )
     }
 
 }
