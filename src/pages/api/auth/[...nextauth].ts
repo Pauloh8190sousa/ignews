55aff3ae25fb61d7f0bf347046a32c3927fd552c
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-08-03 16:22:13 -0300

    autenticação e verificação para liberação de posts completa

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,7 +1,6 @@
 import { query as q } from "faunadb";
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
-
 import { fauna } from "../../../services/faunadb";
 
 export default NextAuth({
@@ -15,6 +14,43 @@ export default NextAuth({
     ],
 
     callbacks: {
+        async session(session) {
+            
+            try {
+                const userActiveSubscription = await fauna.query(
+                    q.Get(
+                        q.Intersection([
+                            q.Match(
+                                q.Index("subscription_by_user_ref"),
+                                q.Select(
+                                    "ref",
+                                    q.Get(
+                                        q.Match(
+                                            q.Index("user_by_email"),
+                                            q.Casefold(session.user.email)
+                                        )
+                                    )
+                                )
+                            ),
+                            q.Match(
+                                q.Index("subscription_by_status"),
+                                "active"
+                            )
+                        ])
+                    )
+                )
+                
+                return {
+                    ...session,
+                    activeSubscription: userActiveSubscription
+                };
+            }catch {
+                return {
+                    ...session,
+                    activeSubscription: null
+                }
+            } 
+        },
         async signIn(user, account, profile){
 
             const { email } = user;
