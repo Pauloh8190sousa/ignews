6ece1ef8c4316eed849ec7aa5455aa5238c63d1e
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-08-04 14:36:10 -0300

    Projeto finalizado!

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
@@ -56,7 +56,7 @@ export default function PostPreview({ post }: PostPreviewProps) {
     );
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
     return {
         paths: [],
         fallback: "blocking"
@@ -85,7 +85,8 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     return {
        props: {
            post,
-       }
+       },
+       redirect: 60 * 30, //30 minutos
    }
 
 }
